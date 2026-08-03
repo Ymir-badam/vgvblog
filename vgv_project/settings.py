88e19247671971f7db1ec04997b63108090457d5
Kundey Vishwanath <129545041+Ymir-badam@users.noreply.github.com>
2026-08-03 14:31:34 +0530

    Update settings.py

--- vgv_project/settings.py
+++ vgv_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-30*bvofim2jz^#i_ep64^iqv=f0fqon3227zgb2*oz#ft(*9v_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver','vgvblog.onrender.com']
 
 
 # Application definition
